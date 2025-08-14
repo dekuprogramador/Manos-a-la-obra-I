bb62fd8b604c4e89cb910d751c10ed4624638f1c
dekuprogramador <santiagoledesma13@gmail.com>
2025-08-14 17:18:12 -0300

    Se sube el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <section>
+          <article>
+            <h2>LISTA DE TAREAS DE SANTIAGO</h2>
+
+            <form>
+              <label for="nombretarea"></label>
+              <input id="nombretarea" name="nombretarea" placeholder="ingrese el nombre" ></input>
+              <button type="button" id="botonañadir" >
+                ADD
+              </button>
+            </form>
+
+            <div id="columnadetareas">
+
+              <div class="tareas" >
+                <div class="tareastexto" >
+                  <input type="checkbox" id="tarea1" />
+                  <label for="tarea1"> TAREA N </label>
+                </div>
+                
+                <button type="button" class="botonbasura" >
+                  🗑
+                </button>
+              </div>
+              
+              <div class="tareas" >
+                <div class="tareastexto" >
+                  <input type="checkbox" id="tarea2" />
+                  <label for="tarea2"> TAREA N-1 </label>
+                </div>
+                
+                <button type="button" class="botonbasura" >
+                  🗑
+                </button>
+              </div>
+              
+              <div class="tareas" >
+                <div class="tareastexto" >
+                  <input type="checkbox" id="tarea3" />
+                 <label for="tarea3"> TAREA K </label>
+                </div>
+                
+                <button type="button" class="botonbasura" >
+                  🗑
+                </button>
+              </div>
+              
+            </div>
+          </article>
+        </section>
+      </main>
+      
+    </>
+  )
+}
+
+export default App
